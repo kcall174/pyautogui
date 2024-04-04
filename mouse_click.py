aff5b6408bfe94404ab1ed953005eb82998e8bb6
Ken <kmacallaghan@gmail.com>
2024-04-04 15:04:52 -0400

    Create mouse_click.py

---
+++ mouse_click.py
@@ -0,0 +1,24 @@
+import pyautogui
+import threading
+import datetime
+
+screenSize= pyautogui.size() # get screensize
+
+def moveMouse(): 
+    pyautogui.moveTo95, screenSize[1], duration = 1
+    
+def clickMouse():
+    pyautogui.click()
+    main()
+    
+def main():
+    hour = datetime.datetime.now().hour
+    if hour == 17 or hour == 12:
+        print('end of day reached')
+        quit()
+        
+    else: 
+        threading.Timer(5.0, moveMouse).start()
+        threading.Timer(10.0, clickMouse).start()
+        
+main()
